e2b4f8b5bc76c8bc38ffcfae644104e82c301381
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2024-11-20 13:25:23 +0600

    initial category data add

---
+++ src/components/Category.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Category = () => {
+    return (
+        <div className='w-fit bg-white text-black'>
+            this is category
+        </div>
+    );
+};
+
+export default Category;
---
+++ src/components/Data/Categories.ts
@@ -0,0 +1,78 @@
+export const categories = [
+    {
+        name: "Womens’ & Girls’ Fashion"
+    },
+    {
+        name: "Health & Beauty"
+    },
+    {
+        name: "Watches, Bags, Jewellery",
+        subCategories:[
+            {
+                name:"kids bags"
+            },
+            {
+                name:"laptop bags & cases"
+            },
+            {
+                name:"Luggage"
+            },
+            {
+                name:"Travel Bags"
+            },
+            {
+                name:"Men’s bag"
+            },
+            {
+                name:"Women’s Bag",
+                nastedSubcategories:[
+                    {
+                        name:"Backpacks"
+                    },
+                    {
+                        name:"Cross Body & Shulder Bags"
+                    },
+                    {
+                        name:"Bag Charms& Accessories"
+                    },
+                    {
+                        name:"Clutches"
+                    },
+                    {
+                        name:"Top-Handle Bags"
+                    },
+                    {
+                        name:"Tote bags"
+                    },
+                    {
+                        name:"wallets"
+                    },
+                ]
+            },
+            {
+                name:"Men’s Watches"
+            },
+            {
+                name:"Women’s Watches"
+            },
+            {
+                name:"Kids Watches"
+            },
+            {
+                name:"women’s jewellery"
+            },
+            {
+                name:"men’s jewellery"
+            },
+            {
+                name:"sunglasses"
+            },
+        ]
+    },
+    {
+
+    },
+    {
+
+    },
+]
