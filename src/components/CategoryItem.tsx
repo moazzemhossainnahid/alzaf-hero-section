1f2e997b3e10a9a450f4724a52c54e01f255f189
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2024-11-21 22:28:00 +0600

    fix card hover design

--- src/components/CategoryItem.tsx
+++ src/components/CategoryItem.tsx
@@ -25,11 +25,24 @@ const CategoryItem: React.FC<CategoryItemProps> = ({ category }) => {
       onMouseLeave={() => setIsHovered(false)}
     >
       <span>{category?.title}</span>
-      {category?.childrens && <AiOutlineRight className="mr-2" />}
 
-      {isHovered && category?.childrens && (
-        <ul className="absolute h-full w-[230px] left-full top-0 bg-white border-x py-4">
-          {category?.childrens?.map((child) => (
+      {/* Icon */}
+      {category?.childrens && (
+        <AiOutlineRight
+          className={`mr-2 transform ${
+            isHovered ? "rotate-180" : "rotate-0"
+          } transition-transform duration-300`}
+        />
+      )}
+
+      {/* Animated Sub-Category */}
+      {category?.childrens && (
+        <ul
+          className={`absolute left-full top-0 h-full w-[230px] bg-white border-x py-4 origin-left transform transition-all duration-300 ease-in-out ${
+            isHovered ? "scale-x-100 opacity-100" : "scale-x-0 opacity-0"
+          }`}
+        >
+          {category?.childrens.map((child) => (
             <CategoryItem key={child?.id} category={child} />
           ))}
         </ul>
